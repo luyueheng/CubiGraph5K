3ce0b14e888caabca5d27ba56612513ac515283e
leoninela <leonine_la@163.com>
2020-11-24 08:08:15 -0500

    add query functions
    Add roomNames, bfs, shortest path

--- src/plan.py
+++ src/plan.py
@@ -127,6 +127,9 @@ class Room:
         room_type_count[self.type] += 1
         self.center_point = self._get_center_point()
         self.adjacent_doors = set()
+        self.neighbours=[]
+        self.level={}
+        self.parent={}
 
     def _get_center_point(self) -> Tuple[float, float]:
         return (mean([p[0] for p in self.points]), mean([p[1] for p in self.points]))
@@ -164,7 +167,12 @@ class Door:
 
 class Plan:
 
-    def __init__(self, svg: bs4Tag):
+    def __init__(self, dataPath,id):
+        self.path=dataPath+'/{}/model.svg'.format(id)
+        with open(self.path) as f:
+            content = f.read(1000000)
+        soup = BeautifulSoup(content, 'lxml')
+        self.svg=soup.find('svg')
         self.rooms = [] # parse from svg
         self.name2room = {}
         self.doors = [] # parse from svg
@@ -172,22 +180,27 @@ class Plan:
         self.relation = [] # generate by calling self.generate_room_relation()
         self.room_type_count = defaultdict(int)
         self.door_count = 0
-        self.svg_dimension = (svg.attrs['height'], svg.attrs['width'])
+        self.svg_dimension = (self.svg.attrs['height'], self.svg.attrs['width'])
+        self.roomNames=[]
+        self.adjList=self.get_adjacency_list()
+        self.vertices={} 
 
-        for r in svg.find_all('g', attrs={'class': 'Space'}):
+        for r in self.svg.find_all('g', attrs={'class': 'Space'}):
             room = Room(r, self.room_type_count)
             self.rooms.append(room)
             self.name2room[room.name] = room
+            self.roomNames.append(room.name)
 
-        for d in svg.find_all('g', attrs={'class': 'Threshold'}):
+        for d in self.svg.find_all('g', attrs={'class': 'Threshold'}):
             door = Door(d, self.door_count)
             self.doors.append(door)
             self.door_count += 1
             self.name2door[door.name] = door
-
+    
     def generate_room_relation(self) -> None:
         for room in self.rooms:
             room.get_adjacent_doors(self.doors)
+            self.add_vertex(room)
         
         for i in range(len(self.rooms)):
             for j in range(i+1, len(self.rooms)):
@@ -200,6 +213,7 @@ class Plan:
                     self.relation.append((room1.name, 2, room2.name))
                 else:
                     self.relation.append((room1.name, 0, room2.name))
+        self.get_neighbours()
 
     def get_adjacency_list(self) -> dict:
         relation_adj_list = {name : {} for name in self.name2room}
@@ -208,6 +222,33 @@ class Plan:
                 relation_adj_list[name1][name2] = label
                 relation_adj_list[name2][name1] = label
         return relation_adj_list
+
+    def get_neighbours(self) -> None:
+        for room in self.rooms:
+            room.get_adjacent_doors(self.doors)
+        
+        for i in range(len(self.rooms)):
+            room1 = self.rooms[i]
+            for j in range(0, len(self.rooms)):
+                room2 = self.rooms[j]
+                if room1.to_shapely_polygon().buffer(1.0).intersection(room2.to_shapely_polygon().buffer(1.0)).area > 5.0:
+                    if room2.name !=room1.name:
+                        room1.neighbours.append(room2)
+                elif room1.adjacent_doors.intersection(room2.adjacent_doors):
+                    if room2.name !=room1.name:
+                        room1.neighbours.append(room2)
+
+    def add_vertex(self,room): # add vertex(room instance) into the graph
+        if isinstance(room, Room) and room.name not in self.vertices:
+            self.vertices[room.name]=room
+            return True
+        else:
+            return False
+     
+    def print_graph(self):
+        for key in list(self.vertices.keys()):
+            print(key + ': '+str([n.name for n in self.vertices[key].neighbours])+'\n')
+                
 
     def generate_relation_svg(self) -> 'Tag':
         attrs = {
@@ -259,24 +300,84 @@ class Plan:
             svg.add(text)
 
         return svg
+    
+    def explore(self,s):
+        levels={s:0}
+        i=1
+        frontier=[s]
+        while frontier:
+            # print(frontier)
+            nxt=[]
+            for u in frontier:
+                for v in u.neighbours:
+                    if v not in levels:
+                        levels[v]=i
+                        v.level=i
+                        v.parent[v.name]=u
+                        nxt.append(v)
+            frontier=nxt
+            i+=1
+            out={}
+            for l in levels:
+                out[l.name]=levels[l]
+        return out
+
+    def bt(self,answers, answer, path):
+        if len(answer)==len(path):
+            answers.append(answer[:])
+            return
+        for p in path[len(answer)]:
+            answer.append(p)
+            self.bt(answers,answer,path)
+            answer.pop()
+    
+    def get_all_paths(self,path):
+        answers, answer=[],[]
+        self.bt(answers, answer, path)
+        return answers
+
+    def shortest_path(self,s,t):
+        self.explore(s)
+        path=[[t.name]]
+        if not t.parent:
+            print('No path')
+            return
+        else:
+            while t.parent[t.name].name!=s.name:
+                nxt=[]
+                for n in t.neighbours:
+                    if n.level==t.level-1 and n.parent[n.name]==t.parent[t.name].parent[t.parent[t.name].name]:
+                        nxt.append(n.name)
+                path.append(nxt)
+                t=t.parent[t.name]
+            path.append([s.name])
+            path.reverse()
+        return self.get_all_paths(path)
 
 
 def main():
-    n = 283
-    input_path = '../data_lyh/cubicasa5k/high_quality_architectural/{}/model.svg'.format(n)
-    output_path = '../data_lyh/vis/{}_room_new.svg'.format(n)
+    n = 41
+    # input_path = '../data_lyh/cubicasa5k/high_quality_architectural/{}/model.svg'.format(n)
+    input_path = 'C:/Users/ao/Desktop/dataProjectI/CubiGraph5K-main/CubiGraph5K-main/data'
+    output_path = 'C:/Users/ao/Desktop/dataProjectI/CubiGraph5K-main/CubiGraph5K-main/data/svg/{}.svg'.format(n)
 
-    with open(input_path) as f:
-        content = f.read(1000000)
-    soup = BeautifulSoup(content, 'lxml')
+    
+    # with open(input_path) as f:
+    #     content = f.read(1000000)
+    # soup = BeautifulSoup(content, 'lxml')
 
-    plan = Plan(soup.find('svg'))
+    plan = Plan(input_path,n)
     plan.generate_room_relation()
     # print(plan.name2room['Entry_1'].to_shapely_polygon().buffer(1.0).intersection(plan.name2room['Kitchen_1'].to_shapely_polygon().buffer(1.0)).area)
     # print(list(plan.name2room['LivingRoom_1'].adjacent_doors)[0])
     # print(plan.name2door[list(plan.name2room['LivingRoom_1'].adjacent_doors)[0]].to_shapely_polygon().buffer(1.0).intersection(plan.name2room['Kitchen_1'].to_shapely_polygon()).area)
     # print(plan.name2door[list(plan.name2room['LivingRoom_1'].adjacent_doors)[0]].to_shapely_polygon().buffer(1.0).intersection(plan.name2room['LivingRoom_1'].to_shapely_polygon()).area)
-    print(plan.get_adjacency_list())
+    # print(plan.roomNames)
+    print(plan.rooms[1].neighbours)
+    plan.print_graph()
+    s=plan.vertices['Bedroom_2']
+    e=plan.vertices['Bedroom_3']
+    print(plan.shortest_path(s,e))
     with open(output_path, 'w') as output:
         output.write(str(plan.generate_relation_svg()))
     
